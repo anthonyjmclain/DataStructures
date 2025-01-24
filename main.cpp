a3a6e13e241a50890f0e4a08fe3e99c6fa05bc49
anthonyjmclain <128003897+anthonyjmclain@users.noreply.github.com>
2025-01-23 21:16:47 -0600

    edited for demonstration purposes

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 /*
-
 This file serves to test the functionality of git
-
+DataStructures - SP25
+Anthony McLain, 1/23/24
 */
 
 #include<iostream>
 
 int main() {
-    std::cout << "SUFFER\n";
+    std::cout << "This is blank.";
     return 0;
 }
